67af14753e77224aa424a3c3a906beb15deabcdd
Epigene <augusts.bautra@gmail.com>
2024-09-10 14:35:58 +0300

    add segment intersection check

--- lib/geometry.rb
+++ lib/geometry.rb
@@ -69,6 +69,41 @@ class Segment
     Math.sqrt(dx**2 + dy**2)
   end
 
+  # Useful in building visibility graph and shortest flight path
+  def intersect?(other_segment)
+    o1 = orientation(p1, p2, other_segment.p1)
+    o2 = orientation(p1, p2, other_segment.p2)
+    o3 = orientation(other_segment.p1, other_segment.p2, p1)
+    o4 = orientation(other_segment.p1, other_segment.p2, p2)
+
+    # General case
+    return true if o1 != o2 && o3 != o4
+
+    # Special cases
+    # p1, p2, and p3 are collinear and p3 lies on segment p1p2
+    if o1 == 0 && on_segment?(p1, p2, other_segment.p1)
+      return true
+    end
+
+    # p1, p2, and p4 are collinear and p4 lies on segment p1p2
+    if o2 == 0 && on_segment?(p1, p2, other_segment.p2)
+      return true
+    end
+
+    # p3, p4, and p1 are collinear and p1 lies on segment p3p4
+    if o3 == 0 && on_segment?(other_segment.p1, other_segment.p2, p1)
+      return true
+    end
+
+    # p3, p4, and p2 are collinear and p2 lies on segment p3p4
+    if o4 == 0 && on_segment?(other_segment.p1, other_segment.p2, p2)
+      return true
+    end
+
+    # Otherwise, the line segments do not intersect
+    false
+  end
+
   # @return [Integer] a rounded angle the segment is facing in, in physics terms:
   # 0;0 -> 100; 1 == 1 degree
   # 0;0 -> 1; 100 == 89 degrees
@@ -121,5 +156,26 @@ class Segment
 
   def hash
     [@p1, @p2].hash
+  end
+
+  private
+
+  # Helper function to check if point p3 lies on line segment p1p2 (for collinear case)
+  def on_segment?(p1, p2, p3)
+    p3.x >= [p1.x, p2.x].min && p3.x <= [p1.x, p2.x].max &&
+    p3.y >= [p1.y, p2.y].min && p3.y <= [p1.y, p2.y].max
+  end
+
+  # Helper function to compute the orientation of triplet (A, B, C)
+  def orientation(p1, p2, p3)
+    val = (p3.y - p2.y) * (p2.x - p1.x) - (p3.x - p2.x) * (p2.y - p1.y)
+
+    if val == 0
+      return 0  # collinear
+    elsif val > 0
+      return 1  # clockwise
+    else
+      return 2  # counterclockwise
+    end
   end
 end
--- spec/segment_spec.rb
+++ spec/segment_spec.rb
@@ -1,4 +1,58 @@
 RSpec.describe Segment do
+  describe "#intersect?(other_segment)" do
+    subject(:intersect?) { segment.intersect?(other_segment) }
+
+    let(:segment) { described_class[Point[10, 10], Point[20, 10]] }
+
+    context "when horizontal line is crossed by a vertical one" do
+      let(:other_segment) { described_class[Point[15, 5], Point[15, 15]] }
+
+      it { is_expected.to be(true) }
+    end
+
+    context "when horizontal line is crossed by a diagonal one" do
+      let(:other_segment) { described_class[Point[9, 11], Point[21, 9]] }
+
+      it { is_expected.to be(true) }
+    end
+
+    context "when one line starts on the other (1 shared vertex)" do
+      let(:other_segment) { described_class[Point[11, 10], Point[11, 5]] }
+
+      it { is_expected.to be(true) }
+    end
+
+    context "when both lines share a vertex (co-origin)" do
+      let(:other_segment) { described_class[Point[10, 10], Point[11, 5]] }
+
+      it { is_expected.to be(true) }
+    end
+
+    context "when both segments are on the same line, but do not overlap" do
+      let(:other_segment) { described_class[Point[21, 10], Point[30, 10]] }
+
+      it { is_expected.to be(false) }
+    end
+
+    context "when both segments are on parallel lines" do
+      let(:other_segment) { described_class[Point[10, 11], Point[15, 11]] }
+
+      it { is_expected.to be(false) }
+    end
+
+    context "when a horizontal line is not crossed by a vertical line further on" do
+      let(:other_segment) { described_class[Point[21, 11], Point[21, 5]] }
+
+      it { is_expected.to be(false) }
+    end
+
+    context "when a horizontal line is not crossed by a vertical line under it" do
+      let(:other_segment) { described_class[Point[15, 9], Point[15, 5]] }
+
+      it { is_expected.to be(false) }
+    end
+  end
+
   describe "#eight_sector_angle" do
     subject(:eight_sector_angle) { segment.eight_sector_angle }
 
